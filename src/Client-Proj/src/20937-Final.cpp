cf794448ef41f642948e0f54bb884edbd3448204
LiamAs05 <liamdrop205@gmail.com>
2023-11-13 19:31:02 +0200

    Added checksum calculation

--- src/Client-Proj/src/20937-Final.cpp
+++ src/Client-Proj/src/20937-Final.cpp
@@ -1,7 +1,9 @@
-#include "Client.h"
+#include "Utils.h"
 
 int main()
 {
 	std::cout << "Client starting..." << std::endl;
-	auto c = Client();
+	//auto c = Client();
+	const char* content = "abcd";
+	std::cout << Utils::memcrc((char*)content, 4) << std::endl;
 }
--- src/Client-Proj/src/Utils.h
+++ src/Client-Proj/src/Utils.h
@@ -8,6 +8,7 @@
 class Utils
 {
 public:
+	static unsigned long memcrc(char* content, size_t size);
 	static std::string read_file(const std::string& path);
 	static void write_file(const std::string& path, const std::string& content);
 	static std::vector<std::string> split_lines(const std::string& content);
